175f9333ae748dedc6a45a18a78477bd1b31c1fb
ZibinChen <zbchen920117@gmail.com>
2026-02-27 12:10:52 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  }
-}
+  output: 'export', // 告诉 Next.js 将项目导出为静态 HTML
+  images: {
+    unoptimized: true, // GitHub Pages 不支持 Next.js 的默认图片优化
+  },
+};
 
-export default nextConfig
+export default nextConfig;
