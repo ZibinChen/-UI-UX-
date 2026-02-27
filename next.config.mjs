34886fe47dbfffc7004683c7d098ed2a4370b203
ZibinChen <zbchen920117@gmail.com>
2026-02-27 12:37:31 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  // 添加下面这几行，让编译跳过类型和代码检查
+  // 关键：告诉 Next.js 你的项目部署在 /-UI-UX-/ 这个子路径下
+  basePath: '/-UI-UX-', 
+  assetPrefix: '/-UI-UX-/',
   typescript: {
     ignoreBuildErrors: true,
   },
