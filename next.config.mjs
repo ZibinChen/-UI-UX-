0605efaf84a9a39fdaff83a4377c965c4f7a6c05
ZibinChen <zbchen920117@gmail.com>
2026-02-27 12:42:52 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  // 必须添加这两行，且名字必须与你的仓库名 /-UI-UX-/ 完全一致
-  basePath: '/-UI-UX-',
+  // 必须和你的仓库名 -UI-UX- 完全一致
+  basePath: '/-UI-UX-', 
   assetPrefix: '/-UI-UX-/', 
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
